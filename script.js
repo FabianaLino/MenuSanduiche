0cb4bbbabe46c7faf5ed68d13facffce8bbcbe56
FabianaLino <fabianalino@icloud.com>
2023-10-02 19:59:33 -0300

    Aançando no cod.

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+function goToHomePage() {
+    var paginaInicial = "index.html";
+    // Redireciona o navegador para a página principal (index.html)
+    window.location.href = paginaInicial;
+}
